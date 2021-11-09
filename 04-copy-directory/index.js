21cdad3b27e0cfe51e0aecc55dd60b26f0eec9c7
SavelyevNikita <nik2111122@sibmail.com>
2021-11-10 01:21:24 +0300

    feat:04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,15 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-// const initPath = path.join(__dirname, 'files');
-// const finalPath = path.join(__dirname, 'files-copy');
 fs.readdir(path.join(__dirname), { withFileTypes: true }, (err, files) => {
     if (err) throw err;
     files.forEach(el => {
+        // console.log(el);
         if (el.name === 'files-copy') {
-            // console.log(el.name);
-            fs.rm(`${path.join(__dirname, 'files-copy')}`, { recursive: true }, () => {
-                // console.log(el.name);
-            });
+            fs.rm(`${path.join(__dirname, 'files-copy')}`, { recursive: true }, () => {});
         }
     });
 });
@@ -17,12 +13,10 @@ fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
     if (err) throw err;
 });
 fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true }, (err, files) => {
-    if (err) { console.log('error'); }
+    if (err) throw err;
     files.forEach(el => {
         fs.copyFile(`${path.join(__dirname, 'files', el.name)}`, `${path.join(__dirname, 'files-copy', el.name)}`, (err) => {
-            if (err) {
-                console.log("Error Found:", err);
-            }
+            if (err) throw err;
         });
     });
 });
