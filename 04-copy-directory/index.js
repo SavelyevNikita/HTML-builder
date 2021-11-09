99fba45a29a9e67eab34e4c3be13d0396a004bcd
SavelyevNikita <nik2111122@sibmail.com>
2021-11-09 23:25:48 +0300

    feat:04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
 const path = require('path');
-const myPath = path.join(__dirname, 'files');
-// console.log(myPath);
+// const initPath = path.join(__dirname, 'files');
+// const finalPath = path.join(__dirname, 'files-copy');
 fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
     if (err) throw err;
 });
-
-fs.readdir(myPath, { withFileTypes: true }, (err, files) => {
+fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true }, (err, files) => {
     if (err) { console.log('error'); }
     files.forEach(el => {
-        fs.copyFile(path.join(__dirname, 'files', el.name, path.join(__dirname, 'files-copy', el.name)), (err) => {
-            if (err) throw err;
-            console.log('source.txt was copied to destination.txt');
+        fs.copyFile(`${path.join(__dirname, 'files', el.name)}`, `${path.join(__dirname, 'files-copy', el.name)}`, (err) => {
+            if (err) {
+                console.log("Error Found:", err);
+            }
         });
         // console.log(path.join(__dirname, 'files', el.name));
         // console.log(path.join(__dirname, 'files-copy', el.name));
