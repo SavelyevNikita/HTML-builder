7b3dcf020a5925add07fc8b6083d9a4ae2798bc5
SavelyevNikita <nik2111122@sibmail.com>
2021-11-10 22:38:10 +0300

    feat:04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,26 +1,13 @@
 const fs = require('fs');
 const fsPromises = fs.promises;
 const path = require('path');
-// fs.readdir(path.join(__dirname), { withFileTypes: true }, (err, files) => {
-// if (err) throw err;
-// files.forEach(el => {
-//     // console.log(el);
-//     if (el.name === 'files-copy') {
 const funcUpdate = async() => {
-        await fsPromises.rm(path.join(__dirname, 'files-copy'), { recursive: true, force: true });
-        //     }
-        // });
-        await fsPromises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
-    }
-    // });
-    // fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
-    //     if (err) throw err;
-    // });
-const createFile = async() => {
-    await funcUpdate();
-    const files = await fsPromises.readdir(path.join(__dirname, 'files'), { withFileTypes: true });
-    files.forEach(el => {
-        fsPromises.copyFile(`${path.join(__dirname, 'files', el.name)}`, `${path.join(__dirname, 'files-copy', el.name)}`);
-    })
-};
-createFile();
+    await fsPromises.rm(path.join(__dirname, 'files-copy'), { recursive: true, force: true });
+    await fsPromises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+    await fsPromises.readdir(path.join(__dirname, 'files'), { withFileTypes: true }).then(files => {
+        files.forEach(el => {
+            fsPromises.copyFile(`${path.join(__dirname, 'files', el.name)}`, `${path.join(__dirname, 'files-copy', el.name)}`);
+        });
+    });
+}
+funcUpdate();
