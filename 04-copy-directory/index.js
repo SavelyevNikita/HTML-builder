46092284c091ddd5d6a307e592bfdb7bf84b90c9
SavelyevNikita <nik2111122@sibmail.com>
2021-11-10 22:24:04 +0300

    feat:04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,22 +1,26 @@
 const fs = require('fs');
+const fsPromises = fs.promises;
 const path = require('path');
-fs.readdir(path.join(__dirname), { withFileTypes: true }, (err, files) => {
-    if (err) throw err;
+// fs.readdir(path.join(__dirname), { withFileTypes: true }, (err, files) => {
+// if (err) throw err;
+// files.forEach(el => {
+//     // console.log(el);
+//     if (el.name === 'files-copy') {
+const funcUpdate = async() => {
+        await fsPromises.rm(path.join(__dirname, 'files-copy'), { recursive: true, force: true });
+        //     }
+        // });
+        await fsPromises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+    }
+    // });
+    // fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    //     if (err) throw err;
+    // });
+const createFile = async() => {
+    await funcUpdate();
+    const files = await fsPromises.readdir(path.join(__dirname, 'files'), { withFileTypes: true });
     files.forEach(el => {
-        // console.log(el);
-        if (el.name === 'files-copy') {
-            fs.rm(`${path.join(__dirname, 'files-copy')}`, { recursive: true }, () => {});
-        }
-    });
-});
-fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
-    if (err) throw err;
-});
-fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true }, (err, files) => {
-    if (err) throw err;
-    files.forEach(el => {
-        fs.copyFile(`${path.join(__dirname, 'files', el.name)}`, `${path.join(__dirname, 'files-copy', el.name)}`, (err) => {
-            if (err) throw err;
-        });
-    });
-});
+        fsPromises.copyFile(`${path.join(__dirname, 'files', el.name)}`, `${path.join(__dirname, 'files-copy', el.name)}`);
+    })
+};
+createFile();
